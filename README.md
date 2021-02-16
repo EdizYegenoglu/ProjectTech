57895c93f8d9eeae3a895d6d312607fffaf4d0dd
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-02-16 12:42:42 +0100

    begonnen met templating

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ProjectTech
+
+## Concept
+Een applicatie waar gebruikers met elkaar kunnen matchen op basis van overeenkomende tags.
+
+## Feature
+Een zoekfunctie om alle gebruikers met die tag te filteren.
--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,49 @@
+// imports 
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Thanks nino!!!')
+
+// static files 
+app.use(express.static('static'))
+app.use('/css', express.static(__dirname + 'static/css'))
+app.use('/js', express.static(__dirname + 'static/js'))
+app.use('/images', express.static(__dirname + 'static/images'))
+
+// set views 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/:userQuery', (req, res) => {
+    res.render('index', {data : {userQuery: req.params.userQuery,
+                                searchResults : ['Albert', 'Bart', 'Chiara']}})
 })
 
+// app.get('/', (req, res) => {
+//     res.render('index', {text: 'this is EJS'})
+// })
+
+app.get('/about', (req, res) => {
+    res.render('about', {text: 'About page'})
+})
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(__dirname + '/static/about.html')
+// })
+
+
+// listen on port 3000 
 app.listen(port, () => {
     console.log('example app listening at http://localhost:3000')
 })
+
+
+// errormessage 
+app.use((req, res) => {
+    // res.status(404).send('404 Not Found')
+    res.sendFile(__dirname + '/static/images/404.png')
+})
+
+
 
 
