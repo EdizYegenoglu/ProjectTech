4bdc3e28ff4d05b0530b7fda2858d23f08bf0091
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-02-25 17:32:44 +0100

    afbeelding toegevoegd

--- myapp/app.js
+++ myapp/app.js
@@ -1,27 +1,49 @@
 // imports 
 const express = require('express')
+const slug = require('slug')
 const app = express()
 const port = 3000
-
+// const bodyParser = require('body-parser')
 
 // static files 
-app.use(express.static('static'))
-app.use('/css', express.static(__dirname + 'static/css'))
-app.use('/js', express.static(__dirname + 'static/js'))
-app.use('/images', express.static(__dirname + 'static/images'))
+app.use(express.static(__dirname + 'static'))
+
 
 // set views 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.get('/:userQuery', (req, res) => {
-    res.render('index', {data : {userQuery: req.params.userQuery,
+
+
+// Page waarop je zoekt naar woord en dat zoekresultaat is 
+// app.get('/:userQuery', (req, res) => {
+//     res.render('index', {data : {userQuery: req.params.userQuery,
+//                                 searchResults : ['Albert', 'Bart', 'Chiara']}})
+// })
+
+app.post('/results', function(req, res){
+    // req.body object has your form values
+    console.log(req.body.userQuery);
+ });
+
+app.get('/', (req, res) => {
+    // res.render('index', {text: 'this is EJS'})
+    // const subject = req.body.subject
+    res.render('index', {data : {
+        // searchSubject: subject,
                                 searchResults : ['Albert', 'Bart', 'Chiara']}})
 })
 
-// app.get('/', (req, res) => {
-//     res.render('index', {text: 'this is EJS'})
-// })
+
+
+
+
+
+
+
+
+
+
 
 app.get('/about', (req, res) => {
     res.render('about', {text: 'About page'})
