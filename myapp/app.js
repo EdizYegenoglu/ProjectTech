ed808203d5e6f025a2b0cd54a8cb19c5c773c4ee
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-01 15:13:14 +0100

    linters

--- myapp/app.js
+++ myapp/app.js
@@ -1,71 +1,57 @@
-// imports 
-const express = require('express')
-const slug = require('slug')
-const app = express()
-const port = 3000
+// imports
+const express = require('express');
+const slug = require('slug');
+
+const app = express();
+const port = 3000;
+const path = require('path');
 // const bodyParser = require('body-parser')
 
-// static files 
-app.use(express.static(__dirname + 'static'))
+// static files
+app.use(express.static(`${__dirname}static`));
+app.use('/static', express.static(path.join(__dirname, '/static')));
 
+// set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-// set views 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-
-
-// Page waarop je zoekt naar woord en dat zoekresultaat is 
+// Page waarop je zoekt naar woord en dat zoekresultaat is
 // app.get('/:userQuery', (req, res) => {
 //     res.render('index', {data : {userQuery: req.params.userQuery,
 //                                 searchResults : ['Albert', 'Bart', 'Chiara']}})
 // })
 
-app.post('/results', function(req, res){
-    // req.body object has your form values
-    console.log(req.body.userQuery);
- });
+app.post('/results', function (req, res) {
+  // req.body object has your form values
+  console.log(req.body.userQuery);
+});
 
 app.get('/', (req, res) => {
-    // res.render('index', {text: 'this is EJS'})
-    // const subject = req.body.subject
-    res.render('index', {data : {
-        // searchSubject: subject,
-                                searchResults : ['Albert', 'Bart', 'Chiara']}})
-})
-
-
-
-
-
-
-
-
-
-
-
+  // res.render('index', {text: 'this is EJS'})
+  // const subject = req.body.subject
+  res.render('index', {
+    data: {
+      // searchSubject: subject,
+      searchResults: ['Albert', 'Bart', 'Chiara'],
+    },
+  });
+});
 
 app.get('/about', (req, res) => {
-    res.render('about', {text: 'About page'})
-})
+  res.render('about', { text: 'About page' });
+});
 
 // app.get('/about', (req, res) => {
 //     res.sendFile(__dirname + '/static/about.html')
 // })
- 
 
-// listen on port 3000 
+// listen on port 3000
 app.listen(port, () => {
-    console.log('example app listening at http://localhost:3000')
-})
+  console.log('example app listening at http://localhost:3000');
+});
 
-
-// errormessage 
-app.use((req, res) => {
-    // res.status(404).send('404 Not Found')
-    res.sendFile(__dirname + '/static/images/404.png')
-})
-
-
-
-
+// errormessage
+// app.use((req, res) => {
+//     // res.status(404).send('404 Not Found')
+//     res.sendFile(__dirname + '/static/images/404.png')
+// });
---
+++ myapp/static/script/script.js
@@ -0,0 +1,75 @@
+// hamburger menu
+const navbar = document.querySelector('header nav');
+const ham = document.querySelector('header button');
+const hamClose = document.querySelector('header ul li');
+const x = document.getElementById('overlayDing');
+
+ham.addEventListener('click', toggleHamburger);
+hamClose.addEventListener('click', toggleHamburger);
+x.addEventListener('click', toggleHamburger);
+
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger() {
+  navbar.classList.toggle('showNav');
+  ham.classList.toggle('showClose');
+  hamClose.classList.toggle('showClose');
+}
+
+const menuLinks = document.querySelectorAll('.menuLink');
+
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener('click', toggleHamburger);
+});
+
+// overlay ding
+ham.addEventListener('click', myFunction);
+hamClose.addEventListener('click', myFunction);
+x.addEventListener('click', myFunction);
+
+const body = document.querySelector('body');
+
+function myFunction() {
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    body.style.overflowY = 'scroll';
+  } else {
+    x.style.display = 'block';
+    body.style.overflowY = 'hidden';
+  }
+}
+
+// dropdown inmenu/
+const dropDown = document.querySelector('nav ul li ul');
+const pijl = document.querySelector('nav ul li svg');
+
+pijl.addEventListener('click', toggleDropDown);
+
+function toggleDropDown() {
+  if (dropDown.style.display === 'block') {
+    dropDown.style.display = 'none';
+    pijl.style.transform = 'rotate(0deg)';
+  } else {
+    dropDown.style.display = 'block';
+    pijl.style.transform = 'rotate(180deg)';
+    pijl.style.transition = '.6s';
+  }
+}
+
+// const dropDownTwee = document.querySelector("#lijstTwee")
+// const pijlTwee = document.querySelector("#pijlTwee")
+
+// pijlTwee.addEventListener("click", toggleDropDownTwee)
+
+// function toggleDropDownTwee(){
+//   if (dropDownTwee.style.display === "block") {
+//     dropDownTwee.style.display = "none";
+//     pijlTwee.style.transform = "rotate(0deg)";
+//   }
+
+//   else {
+//     dropDownTwee.style.display = "block";
+//     pijlTwee.style.transform = "rotate(180deg)";
+//     pijlTwee.style.transition = ".6s";
+//   }
+// }
+// einde dropdown in menu
