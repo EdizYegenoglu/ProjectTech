62fb6163f42476d1a361c039c0d892c1db181e1e
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-04 12:14:25 +0100

    mongodb connected succesfully

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,14 @@
 // imports
 const express = require('express');
 const slug = require('slug');
-
 const app = express();
 const port = 3000;
 const path = require('path');
-// const bodyParser = require('body-parser')
+
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser());
 
 // static files
 app.use(express.static(`${__dirname}static`));
@@ -15,43 +18,29 @@ app.use('/static', express.static(path.join(__dirname, '/static')));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// Page waarop je zoekt naar woord en dat zoekresultaat is
-// app.get('/:userQuery', (req, res) => {
-//     res.render('index', {data : {userQuery: req.params.userQuery,
-//                                 searchResults : ['Albert', 'Bart', 'Chiara']}})
-// })
-
-app.post('/results', function (req, res) {
-  // req.body object has your form values
-  console.log(req.body.userQuery);
-});
-
 app.get('/', (req, res) => {
-  // res.render('index', {text: 'this is EJS'})
-  // const subject = req.body.subject
-  res.render('index', {
-    data: {
-      // searchSubject: subject,
-      searchResults: ['Albert', 'Bart', 'Chiara'],
-    },
-  });
+  res.render('index')
+}); 
+
+
+
+app.post('/results', (req, res) => {
+    const subject = req.body.subject
+    // res.end(JSON.stringify(req.body));
+    res.render('results', {
+        data: {
+          searchSubject: subject,
+          searchResults: ['albert', 'bert']
+        },
+      });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { text: 'About page' });
-});
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(__dirname + '/static/about.html')
-// })
+// app.post('/results', function (req, res) {
+//     req.body object has your form values
+//     console.log(req.body.userQuery);
+//   });
 
 // listen on port 3000
 app.listen(port, () => {
   console.log('example app listening at http://localhost:3000');
 });
-
-// errormessage
-// app.use((req, res) => {
-//     // res.status(404).send('404 Not Found')
-//     res.sendFile(__dirname + '/static/images/404.png')
-// });
---
+++ myapp/database.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
