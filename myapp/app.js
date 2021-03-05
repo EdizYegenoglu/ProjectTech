da662ffa410f446db89a144108bb12801ccf387a
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-05 16:18:30 +0100

    database proberen werkend te krijgen

--- myapp/app.js
+++ myapp/app.js
@@ -5,43 +5,73 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true}));
+const dotenv = require("dotenv").config();
+const { MongoClient } = require('mongodb');
 
 // static files
 app.use(express.static(`${__dirname}static`));
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
-
-// create data schema 
-// const notesSchema = {
-//   subject: String
-// }
-
-// const Note = MongoClient.model("Note", notesSchema);
-
 // set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
+let db = null;
+
+async function connectDB() {
+  async function connectDB() {
+    //get URI form env file
+    const uri = process.env.ATLAS_URI
+    //make connection to DB
+    const options = { useUnifiedTopology: true };
+    const client = new MongoClient(uri, options)
+    await client.connect();
+    db = await client.db(process.env.ATLAS_URI)
+  }
+  connectDB(); 
+    try {
+      console.log('We have made a connection to Mongo!')
+    }   
+    catch (error) {
+      console.log(error)
+    }
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.get("/", async (req, res) => {
+    let subject= {}
+    subject = await db.collection('subject').find({});
+      res.render('index', {
+        data: { searchSubject: subject }
+    });
+  });
+
+  app.post("/results", async (req, res) => {
+    const subject = {}
+    subject = await db.collection('subject').find({});
+    const searchSubject = subject.filter(function (subject) {
+      return subject = (req.body.subject)
+    });
+  });
+};
 
 app.get('/', (req, res) => {
   res.render('index')
-  // let newNote = new Note({
-  //   title: req.body.subject,
-  // })
-  // newNote.save();
 }); 
 
 
-app.post('/results', (req, res) => {
-    const subject = req.body.subject
-    res.render('results', {
-        data: {
-          searchSubject: subject,
-          searchResults: ['albert', 'bert']
-        },
-      });
-});
+
+// app.post('/results', (req, res) => {
+//     const subject = req.body.subject
+//     res.render('results', {
+//         data: {
+//           searchSubject: subject,
+//           searchResults: ['albert', 'bert']
+//         },
+//       });
+// });
 
 // listen on port 3000
 app.listen(port, () => {
--- myapp/database.js
+++ myapp/database.js
@@ -1,18 +1,23 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = process.env.ATLAS_URI;
+// const client = new MongoClient(uri, { useUnifiedTopology: true });
+// async function run() {
+//   try {
+//     // Connect the client to the server
+//     await client.connect();
+//     // Establish and verify connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected successfully to server");
+//   } 
+//   catch (err) {
+//     console.log(err.stack);
+//   }
+  
+//   finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
