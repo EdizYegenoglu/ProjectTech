3cba86d28e3b87cea441a767e15c7e4d66f2ab03
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-05 19:26:23 +0100

    nog steeds database

--- myapp/app.js
+++ myapp/app.js
@@ -58,22 +58,6 @@ async function connectDB() {
     });
   });
 
-  // app.post('/results', (req, res) => {
-  //   res.render("results")
-  // }); 
-
-
-
-// app.post('/results', (req, res) => {
-//     const subject = req.body.subject
-//     res.render('results', {
-//         data: {
-//           searchSubject: subject,
-//           searchResults: ['albert', 'bert']
-//         },
-//       });
-// });
-
 // listen on port 3000
 app.listen(port, () => {
   console.log('example app listening at http://localhost:3000');
