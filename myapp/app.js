b5ab76e012cbae412fb1e2a9e1c357280ddf7c0c
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-05 15:03:09 +0100

    bezig met database fixen

--- myapp/app.js
+++ myapp/app.js
@@ -4,15 +4,21 @@ const slug = require('slug');
 const app = express();
 const port = 3000;
 const path = require('path');
-
-
 const bodyParser = require('body-parser');
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // static files
 app.use(express.static(`${__dirname}static`));
 app.use('/static', express.static(path.join(__dirname, '/static')));
+
+
+// create data schema 
+// const notesSchema = {
+//   subject: String
+// }
+
+// const Note = MongoClient.model("Note", notesSchema);
 
 // set views
 app.set('views', './views');
@@ -20,13 +26,15 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index')
+  // let newNote = new Note({
+  //   title: req.body.subject,
+  // })
+  // newNote.save();
 }); 
-
 
 
 app.post('/results', (req, res) => {
     const subject = req.body.subject
-    // res.end(JSON.stringify(req.body));
     res.render('results', {
         data: {
           searchSubject: subject,
@@ -34,11 +42,6 @@ app.post('/results', (req, res) => {
         },
       });
 });
-
-// app.post('/results', function (req, res) {
-//     req.body object has your form values
-//     console.log(req.body.userQuery);
-//   });
 
 // listen on port 3000
 app.listen(port, () => {
