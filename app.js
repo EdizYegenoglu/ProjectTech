c7c7886a0d4ea26a8610c92cd27882756eb2b978
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-03-08 23:08:36 +0100

    History werkt! nu nog verwijderen.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// imports
 const express = require('express');
 const slug = require('slug');
 const app = express();
@@ -7,90 +6,71 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv").config();
 const { MongoClient } = require('mongodb');
-
 // static files
 app.use(express.static(`${__dirname}static`));
 app.use('/static', express.static(path.join(__dirname, '/static')));
-
 // set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
 let db = null;
-
 async function connectDB() {
-    //get URI form env file
-    const uri = process.env.DB_URI 
-    //make connection to DB
-    const options = { useUnifiedTopology: true };
-    const client = new MongoClient(uri, options)
-    await client.connect();
-    db = await client.db(process.env.DB_NAME) 
-  }
-  connectDB(); 
-    try {
-      console.log('We have made a connection to Mongo!')
-    }   
-    catch (error) {
-      console.log(error)
-    }
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  })); 
-
-  app.get("/", async (req, res) => {
-    let Accounts= {}  
-    Accounts = await db.collection('Accounts').find({}).toArray();
-    res.render('index', {
-      results: 0
-    })
-  }); 
-
-
-  app.get("/history", async (req, res) => {
-    let searchHistory= {}
-    searchHistory = await db.collection('searchHistory').find({}).toArray();
-    const historyItem = searchHistory.filter(function(searchHistory) {
-      return searchHistory.subject.includes(req.body.subject)
-    })
-  res.render('history', {
-    // history: res,
-    historyItem: historyItem,
+  //get URI form env file
+  const uri = process.env.DB_URI
+  //make connection to DB
+  const options = { useUnifiedTopology: true };
+  const client = new MongoClient(uri, options)
+  await client.connect();
+  db = await client.db(process.env.DB_NAME)
+}
+connectDB();
+try {
+  console.log('We have made a connection to Mongo!')
+}
+catch (error) {
+  console.log(error)
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get("/", async (req, res) => {
+  let Accounts = {}
+  Accounts = await db.collection('Accounts').find({}).toArray();
+  res.render('index', {
+    results: 0
   })
-})
-
-
-  app.post("/results", async (req, res) => {
-    Accounts = await db.collection('Accounts').find({}).toArray();
-
-    const searched = (req.body.subject)
-    const filteredSubject = Accounts.filter(function (Accounts) {
-      return Accounts.subject.includes(req.body.subject)
-    });
-  res.render('results', { 
-    results: filteredSubject.length, 
-    searchSubject: searched,
-    list: filteredSubject      
-  }); 
-
-
-
-  app.post("/history", async (req, res) => {
-
-        // toevoegen van gezochte onderwerp naar database 
-        const searchHistory = db.collection('searchHistory');
-        searchHistory.insertOne( {subject: req.body.subject} )
+});
+app.post("/results", async (req, res) => {
+  Accounts = await db.collection('Accounts').find({}).toArray();
+  const searched = (req.body.subject)
+  const filteredSubject = Accounts.filter(function (Accounts) {
+    return Accounts.subject.includes(req.body.subject)
   });
+  // toevoegen van gezochte onderwerp naar database 
+  const searchHistory = db.collection('searchHistory');
+  searchHistory.insertOne({ subject: req.body.subject })
+  res.render('results', {
+    results: filteredSubject.length,
+    searchSubject: searched,
+    list: filteredSubject
+  });
+
 });
-
-
+app.get("/history", async (req, res) => {
+  let searchHistory = {}
+  searchHistory = await db.collection('searchHistory').find().toArray();
+  const historyItem = searchHistory.filter(function (searchHistory) {
+    return searchHistory.subject
+  })
+  res.render('history', {
+    history: res,
+    historyItem: historyItem
+  })
+});
 //404
-app.use((req, res) => {
-  res.status(404).send("this page does not exist.");
-});
-
+// app.use((req, res) => {
+//   res.status(404).send("this page does not exist.");
+// });
 // listen on port 4000
 app.listen(port, () => {
   console.log('example app listening at http://localhost:4000');
